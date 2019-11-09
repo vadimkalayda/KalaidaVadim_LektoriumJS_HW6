6489a488dcd99539296985feb6ea57604953267a
VadimKalayda <vadosgta1@gmail.com>
2019-11-09 14:11:50 +0200

    add isPrime(), factorial, reverse method

--- Task 3/js/index.js
+++ Task 3/js/index.js
@@ -1,5 +1,25 @@
 // isPrime - Returns true or false, indicating whether the given number is prime.
+function isPrime(num) {
+  for(let i = 2; i < num; i++) {
+    if(num % i === 0) {
+      return false;
+    }
+  }
+  if(num > 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 // factorial - Returns a number that is the factorial of the given number.
+function myFactorial(num) {
+  let factorialNumber = 1;
+  for(let i = 1; i <= num; i++) {
+    factorialNumber *= i;
+  }
+  return factorialNumber;
+}
 // fib - Returns the nth Fibonacci number.
 // isSorted - Returns true or false, indicating whether the given array of numbers is sorted.
 
@@ -15,3 +35,12 @@ function myReverse(str) {
 // indexOf - Implement the indexOf function for arrays.
 // isPalindrome - Return true or false indicating whether the given string is a plaindrone (case and space insensitive).
 // missing - Takes an unsorted array of unique numbers (ie. no repeats) from 1 through some number n, and returns the missing number in the sequence (there are either no missing numbers, or exactly one missing number). Can you do it in O(N) time? Hint: There’s a clever formula you can use.
+
+console.log('isPrime(number) - check a number is prime or not');
+console.log('myFactorial(number) - show factorial of a number');
+//console.log() fib
+//console.log() isSorted
+console.log('myReverse(string) - reverse method');
+//console.log() indexOf
+//console.log() isPalindrome
+//console.log() missing
